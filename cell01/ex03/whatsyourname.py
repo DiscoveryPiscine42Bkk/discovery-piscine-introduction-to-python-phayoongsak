ddecce9137c7e1a5f4862f767748e2579d57bee2
phayoongsak <sf6753268@gmail.com>
2025-07-21 09:02:43 +0000

    whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+first_name = input("Hey, what's your first name? : Phayoongsak ")
+last_name = input("And your last name? : Malaikeaw ")
+print("Well, pleased to meet you, Phayoongsak Malaikeaw", first_name, last_name)
